22e42fbd60084d6251ede2e16af11c31137faa4e
EdwinTinoco <jaredlotic@gmail.com>
2020-02-25 14:05:44 -0700

    Correct example for dynamic reduce

--- dynamic_reduce.py
+++ dynamic_reduce.py
@@ -1,23 +1,42 @@
+# import operator
+# from functools import reduce
+
+# def dynamic_reducer(num_one, num_two, num_three, operator_symbol):
+#     if operator_symbol == "+":
+#         print("Operation: Addition")
+#         result = num_one + num_two  + num_three
+#         print(result)
+#     elif operator_symbol == "-":
+#         print("Operation: Sustraction")
+#         result = num_one - num_two - num_three
+#         print(result)
+#     elif operator_symbol == "*":
+#         print("Operation: Multiplication")
+#         result = num_one * num_two * num_three
+#         print(result)
+#     elif operator_symbol == "/":
+#         print("Operation: Division")
+#         result = num_one / num_two / num_three
+#         print(result)    
+    
+
+# dynamic_reducer(1,2,3,"*")
+
+
 import operator
 from functools import reduce
 
-def dynamic_reducer(num_one, num_two, num_three, operator_symbol):
-    if operator_symbol == "+":
-        print("Operation: Addition")
-        result = num_one + num_two  + num_three
-        print(result)
-    elif operator_symbol == "-":
-        print("Operation: Sustraction")
-        result = num_one - num_two - num_three
-        print(result)
-    elif operator_symbol == "*":
-        print("Operation: Multiplication")
-        result = num_one * num_two * num_three
-        print(result)
-    elif operator_symbol == "/":
-        print("Operation: Division")
-        result = num_one / num_two / num_three
-        print(result)    
-    
+def flexible_counter(collection, op):
+    operators = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv,
+    }
 
-dynamic_reducer(1,2,3,"*")
+    return reduce((lambda total, element: operators[op](total, element)), collection)
+
+print(flexible_counter([1,2,3], "+"))
+print(flexible_counter([1,2,3], "-"))
+print(flexible_counter([1,2,3], "*"))
+print(flexible_counter([1,2,3], "/"))
