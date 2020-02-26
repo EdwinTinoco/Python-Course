d91e54129f08d4c2d401d44a5b981d5018cc9ba2
EdwinTinoco <jaredlotic@gmail.com>
2020-02-26 11:00:11 -0700

    Math operations, numbers datatypes

---
+++ Numbers/math_operations.py
@@ -0,0 +1,23 @@
+print('Addition')
+print(100 + 42)
+
+print('Subtraction')
+print(100 - 42.3)
+
+print('Division')
+print(100 / 42)
+print(100 / 38)
+
+print('Floor Division') #redondea el valor a la parte entera
+print(100 // 42)
+print(100 // 38)
+
+print('Multiplication')
+print(100 * 38)
+
+print('Modulus') # te da el residuo y se usa para saber si es par o impar (even es par, odd es impar)
+print(2 % 2)
+print(25 % 2)
+
+print('Exponents')
+print(11 ** 2)
