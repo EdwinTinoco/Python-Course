dcd8ea0f642de3f9a67106107a992c6ee121a3c0
EdwinTinoco <jaredlotic@gmail.com>
2020-02-27 12:43:25 -0700

    Slice class, add to a list

--- Lists/challenge_median_in_list.py
+++ Lists/challenge_median_in_list.py
@@ -20,3 +20,30 @@ sale_prices = [
   3
 ]
 
+#Jordan
+sorted_list = sorted(sale_prices)
+num_of_sales = len(sorted_list) #9
+
+half_slice = math.floor(num_of_sales/2)
+print(half_slice)
+
+first_sales_items = sorted_list[:half_slice]
+last_sales_items = sorted_list[-(half_slice):]
+
+median = sorted_list[half_slice:(half_slice + 1)]
+
+print(sorted_list)
+print(num_of_sales)
+print(first_sales_items)
+print(last_sales_items)
+print(median)
+
+
+#Ryan example
+def find_median_odd(num_list):
+  sorted_list = sorted(num_list)
+  list_len_mid = len(num_list)/2
+
+  return sorted_list[math.floor(list_len_mid)]
+
+print(find_median_odd(sale_prices))
--- Lists/challenge_reverse_word.py
+++ Lists/challenge_reverse_word.py
@@ -22,7 +22,3 @@ def reversed_word(word):
     return word[::-1] 
     
 print(reversed_word("Lucy"))
-
-
-        
-
